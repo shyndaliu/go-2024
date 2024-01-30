9dd3ddca34d1d7f4f785882cd07f7b62edab5970
uldana shyndali <122513545+shyndaliu@users.noreply.github.com>
2024-01-30 16:10:07 +0600

    added routing & experimented with html responses

--- tsis1-aot-api/cmd/aot-api/main.go
+++ tsis1-aot-api/cmd/aot-api/main.go
@@ -16,6 +16,7 @@ func main() {
 
 	router.HandleFunc("/health-check", handlers.HealthCheck).Methods("GET")
 	router.HandleFunc("/titans", handlers.Titans).Methods("GET")
+	router.HandleFunc("/titans/{titan}", handlers.Titan).Methods("GET")
 	http.Handle("/", router)
 
 	http.ListenAndServe(":8080", router)
--- tsis1-aot-api/pkg/handlers/handler.go
+++ tsis1-aot-api/pkg/handlers/handler.go
@@ -5,8 +5,29 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 	"tsis1-aot-api/api"
+
+	"github.com/gorilla/mux"
 )
+
+func generateHTML(t api.Titan) string {
+	abilitiesList := ""
+	for _, ability := range t.Abilities {
+		abilitiesList += fmt.Sprintf("<li>%s</li>\n", ability)
+	}
+	html := fmt.Sprintf(`<img src="%s">
+	<h1>%s</h1>
+	<p><strong>Height:</strong>%d</p>
+	<h2>Abilities:</h2>
+	<ul>
+	%s
+	</ul>
+	<p><strong>Ally:</strong>%s</p>`,
+		t.Image, t.Name, t.Height, abilitiesList, t.Ally)
+
+	return html
+}
 
 func HealthCheck(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
@@ -14,7 +35,7 @@ func HealthCheck(w http.ResponseWriter, r *http.Request) {
 }
 
 func Titans(w http.ResponseWriter, r *http.Request) {
-	log.Println("entering persons end point")
+	log.Println("entering titans end point")
 	titans := api.Titans
 
 	w.Header().Set("Content-Type", "application/json")
@@ -25,4 +46,26 @@ func Titans(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.Write(jsonResponse)
+}
+func Titan(w http.ResponseWriter, r *http.Request) {
+	log.Println("entering titan end point")
+	w.Header().Set("Content-Type", "text/html")
+	titans := api.Titans
+	vars := mux.Vars(r)
+	i, err := strconv.Atoi(vars["titan"])
+	if err != nil || i < 1 || i > 9 {
+		fmt.Fprintf(w, `<h1>404 page not found</h1>`)
+		return
+	}
+	titan := titans[i-1]
+
+	fmt.Fprintf(w, generateHTML(titan))
+
+	// w.Header().Set("Content-Type", "application/json")
+	// w.WriteHeader(http.StatusOK)
+	// jsonResponse, err := json.Marshal(titans)
+	// if err != nil {
+	// 	return
+	// }
+	// w.Write(jsonResponse)
 }
